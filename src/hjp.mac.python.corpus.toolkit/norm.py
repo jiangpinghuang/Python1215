2b000ab514692c47a87733d1b91b9356a2f0c422
hjpwhu <hjpwhu@qq.com>
2016-09-06 15:18:42 +0800

    ini

--- src/hjp.mac.python.corpus.toolkit/norm.py
+++ src/hjp.mac.python.corpus.toolkit/norm.py
@@ -14,27 +14,27 @@ def term2Norm(srcPath, srcFile, targFile):
                 sentt = str[2]                
             else:
                 if str[0] in punc:
-                    sentc = sentc + str[0]
-                    sentt = sentt + str[2]
+                    sentc = sentc
+                    sentt = sentt
                 else:
                     sentc = sentc + " " + str[0]
                     sentt = sentt + " " + str[2]
         if len(line.strip()) == 0:
-            sentc = sentc.replace(' !!', '!!')
-            sentc = sentc.replace(' ..', '..')
-            sentc = sentc.replace(' ...', '...')
-            sentt = sentt.replace(' !!', '!!')
-            sentt = sentt.replace(' ..', '..')
-            sentt = sentt.replace(' ...', '...')
+            #sentc = sentc.replace(' !!', '!!')
+            #sentc = sentc.replace(' ..', '..')
+            #sentc = sentc.replace(' ...', '...')
+            #sentt = sentt.replace(' !!', '!!')
+            #sentt = sentt.replace(' ..', '..')
+            #sentt = sentt.replace(' ...', '...')
             print(sentc)
             print(sentt)
-            src.write(sentc + '\n')
-            tar.write(sentt + '\n')
+            src.write(sentc + ' \n')
+            tar.write(sentt + ' \n')
             sentc = ''
             sentt = ''
         
 if __name__ == '__main__':
-    srcPath = '../../../../../Workshop/Model/data/norm/Normalization_POS_test_set_2.txt'
-    srcFile = '../../../../../Workshop/Model/data/tmp/Normalization_POS_test_set_2_informal.txt'
-    tarFile = '../../../../../Workshop/Model/data/tmp/Normalization_POS_test_set_2_normalization.txt'
+    srcPath = '../../../../../Workshop/Model/data/norm/tweets_normalization_ner.txt'
+    srcFile = '../../../../../Workshop/Model/data/tmp/tweets_normalization_ner_informal.txt'
+    tarFile = '../../../../../Workshop/Model/data/tmp/tweets_normalization_ner_normalization.txt'
     term2Norm(srcPath, srcFile, tarFile)
